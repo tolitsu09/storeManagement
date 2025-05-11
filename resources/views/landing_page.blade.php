7cc96e67fb1491a7d3415a60af68931cdf2c89d9
tolitsu09 <tolitsuuu@gmail.com>
2025-05-11 09:23:14 +0800

    Title

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Shop.CO Landing Page</title>
+  <title>Tolit'Store</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   <style>
